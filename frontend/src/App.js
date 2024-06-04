668572b23c59b136419ba6440a5d1d7435ba8a9e
shanuka_2242 <shanukapayoe@gmail.com>
2024-06-04 23:01:22 +0530

    Misaligned Password Reset Form bug fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,10 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Edit <code>src/App.js</code> and save to reload.
           Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
